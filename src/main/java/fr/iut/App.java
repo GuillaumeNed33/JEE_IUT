669b449e22142a80e5deb8cf4c11972e2b4d447c
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-02-06 19:33:30 +0100

    Ajout Annotations sur chaque classe.

--- src/main/java/fr/iut/App.java
+++ src/main/java/fr/iut/App.java
@@ -1,19 +1,25 @@
 package fr.iut;
-import javax.validation.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+
+import bean.Person;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.util.Set;
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
+
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         Validator validator = factory.getValidator();
-        System.out.println( "Hello World!" );
+
+        Person p1 = new Person();
+        p1.setLogin("");
+
+
+
     }
 }
