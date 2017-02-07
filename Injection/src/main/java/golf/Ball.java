01feb16e6d7b7632ce2d676f461468f41bfb44c5
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-02-07 09:55:27 +0100

    Ajout golf Version Ex1

---
+++ Injection/src/main/java/golf/Ball.java
@@ -0,0 +1,34 @@
+package golf;
+
+import java.awt.geom.Point2D;
+/**
+ * Golf ball class.
+ */
+public class Ball {
+    private final Point2D position = new Point2D.Double();
+
+    /**
+     * default constructor with a position of (0,0)
+     */
+    public Ball() {
+        position.setLocation(0, 0);
+    }
+
+    /**
+     * anemic getter
+     *
+     * @return the current position
+     */
+    public Point2D getPosition() {
+        return position;
+    }
+
+    /**
+     * anemic setter
+     *
+     * @param position the new position to set
+     */
+    public void setPosition(final Point2D position) {
+        this.position.setLocation(position);
+    }
+}
---
+++ Injection/src/main/java/golf/Conditions.java
@@ -0,0 +1,10 @@
+package golf;
+
+/**
+ * Created by Thomas on 07/02/2017.
+ */
+public enum Conditions {
+    GREEN,
+    FAIRWAY,
+    BUNKER
+}
